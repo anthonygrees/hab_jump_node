dd4db1d7ad1cbe33ce87033f1f9296752d57010a
Anthony Rees <anthony_rees01@hotmail.com>
2018-10-03 15:14:24 +1000

    add git

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 bash 'Do some chef pre-work' do
     code <<-EOH
+    sudo yum install git
     curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
     sudo groupadd hab
     sudo useradd -g hab hab
