27e3da9def1975577be5894b65d98c15be448333
Anthony Rees <anthony_rees01@hotmail.com>
2018-10-03 17:16:56 +1000

    docker

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,9 +5,13 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 bash 'Do some chef pre-work' do
     code <<-EOH
+    sudo yum update
     sudo yum install git
     sudo yum install tree
-    sudo yum install docker
+    sudo yum install yum-utils device-mapper-persistent-data lvm2
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum install docker-ce
+    sudo systemctl start docker
     curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
     sudo groupadd hab
     sudo useradd -g hab hab
